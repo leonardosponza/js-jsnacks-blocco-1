b6f5c713b313ac52118d11397cfddd307fccb8b6
leonardo <sponzal4@gmail.com>
2021-06-01 16:06:03 +0200

    secondosnack while

--- js/script.js
+++ js/script.js
@@ -14,12 +14,22 @@
 
 // snack2
 
-for (var i = 0; i < 5 ; i++) {
-      numero = parseInt(prompt('inserisci numero'));
-      console.log(numero);
-      somma +- numero;
-     }
-     document.getElementById('somma').innerHTML = somma;
-     console.log(somma);
+// for (var i = 0; i < 5 ; i++) {
+//       numero = parseInt(prompt('inserisci numero'));
+//       console.log(numero);
+//       somma += numero;
+//      }
+//      document.getElementById('somma').innerHTML = somma;
+//      console.log(somma);
     
-     var somma = 0;
+//      var i = 0
+
+while(i < 5){
+       numero = parseInt(prompt('inserisci numero'));
+        somma += numero;
+        i++;
+}
+  document.getElementById('somma').innerHTML = somma;
+  console.log(somma);
+  
+  var i = 0
