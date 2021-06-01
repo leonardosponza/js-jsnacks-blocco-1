e197cbc84eb42379014e9b9c10c9a934f335cd9d
leonardo <sponzal4@gmail.com>
2021-06-01 16:02:22 +0200

    snack2 for

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 // snack1
-var prima = prompt ('inserisci testo');
-var seconda = prompt ('inserisci testo');
+// var prima = prompt ('inserisci testo');
+// var seconda = prompt ('inserisci testo');
 
-var primalunghezza = seconda.length;
-var secondalunghezza = prima.length;
+// var primalunghezza = seconda.length;
+// var secondalunghezza = prima.length;
 
-if(prima < seconda){
-    document.getElementById('nomi').innerHTML = prima + ' ' + seconda;
-} else if (seconda < prima){
-    document.getElementById('nomi').innerHTML = seconda + ' ' + prima;
-}
+// if(prima < seconda){
+//     document.getElementById('nomi').innerHTML = prima + ' ' + seconda;
+// } else if (seconda < prima){
+//     document.getElementById('nomi').innerHTML = seconda + ' ' + prima;
+// }
+
+
+// snack2
+
+for (var i = 0; i < 5 ; i++) {
+      numero = parseInt(prompt('inserisci numero'));
+      console.log(numero);
+      somma +- numero;
+     }
+     document.getElementById('somma').innerHTML = somma;
+     console.log(somma);
+    
+     var somma = 0;
